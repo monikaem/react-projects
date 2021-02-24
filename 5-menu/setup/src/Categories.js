b1483d29c286d0e397b7477a7e39dff1f8c34a30
Monika <monika.machula94@gmail.com>
2021-02-24 11:39:22 +0100

    add Menu Project

--- 5-menu/setup/src/Categories.js
+++ 5-menu/setup/src/Categories.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({filterItems, categories}) => {
+  const categoriesButtons = categories.map((category, index) => {
+      return <button key={index} className='filter-btn' onClick={() => filterItems(category)}>{category}</button>
+    })
+  return (
+      <div className='btn-container'>
+        {categoriesButtons}
+      </div>
+  )
 };
 
 export default Categories;

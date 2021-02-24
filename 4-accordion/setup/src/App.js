7a74a7b98bbb726eebde9282e1ed0891c618f6e6
Monika <monika.machula94@gmail.com>
2021-02-24 10:41:16 +0100

    add accordion project

--- 4-accordion/setup/src/App.js
+++ 4-accordion/setup/src/App.js
@@ -1,8 +1,24 @@
 import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
+
+
 function App() {
-  return <h2>accordion project setup</h2>;
+  const questions = data.map(question => {
+    return <SingleQuestion key={question.id} {...question}></SingleQuestion>
+  })
+
+  return (
+      <main>
+      <div className='container'>
+          <h3>questions and answers about login</h3>
+          <section className='info'>
+              {questions}
+          </section>
+      </div>
+      </main>
+  )
+
 }
 
 export default App;

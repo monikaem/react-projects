db08686e9b7390bcd5eb49f623e495b1e8651767
Monika <monika.machula94@gmail.com>
2021-03-05 15:39:46 +0100

    add Grocery Bud project

--- 10-grocery-bud/setup/src/List.js
+++ 10-grocery-bud/setup/src/List.js
@@ -1,7 +1,20 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
+const List = ({items, removeItem, editItem}) => {
+  return (
+      <div className='grocery-list'>
+          {items.map(item => {
+            const {id, title} = item;
+            return <article key={id} className='grocery-item'>
+                <p className='title'>{title}</p>
+                <div className='btn-container'>
+                  <button type='button' onClick={ () => editItem(id)} className='edit-btn'><FaEdit/></button>
+                  <button type='button' onClick={() => removeItem(id)} className='delete-btn'><FaTrash/></button>
+                </div>
+            </article>
+          })}
+      </div>
+  )
 }
 
 export default List

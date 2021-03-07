5dfb9cf15e303f728e362979ff12ba688f6e0c57
Monika <monika.machula94@gmail.com>
2021-03-07 14:26:18 +0100

    add stripe menu project

--- 13-stripe-submenus/setup/src/Navbar.js
+++ 13-stripe-submenus/setup/src/Navbar.js
@@ -1,9 +1,36 @@
 import React from 'react'
 import logo from './images/logo.svg'
 import { FaBars } from 'react-icons/fa'
+import {useGlobalContext} from "./context";
 
 const Navbar = () => {
-  return <h2>navbar component</h2>
+  const {openSidebar, openSubmenu, closeSubmenu} = useGlobalContext();
+    const displaySubmenu = (e) => {
+        const page = e.target.textContent;
+        const tempBtn = e.target.getBoundingClientRect();
+        const center = (tempBtn.left + tempBtn.right)/2;
+        const bottom = tempBtn.bottom - 3;
+        openSubmenu(page, {center, bottom});
+    }
+    const handleSubmenu = (e) => {
+        if (!e.target.classList.contains('link-btn')){
+            closeSubmenu();
+        }
+    }
+  return (
+      <nav className='nav' onMouseOver={handleSubmenu}>
+        <div className='nav-header'>
+          <img src={logo} alt='stripe' className='nav-logo'/>
+          <button className='btn toggle-btn' onClick={openSidebar}><FaBars/></button>
+        </div>
+        <ul className='nav-links'>
+            <li><button onMouseOver={displaySubmenu} className='link-btn'>products</button></li>
+            <li><button onMouseOver={displaySubmenu} className='link-btn'>developers</button></li>
+            <li><button onMouseOver={displaySubmenu} className='link-btn'>company</button></li>
+        </ul>
+          <button className='btn signin-btn'>Sign in</button>
+      </nav>
+  )
 }
 
 export default Navbar

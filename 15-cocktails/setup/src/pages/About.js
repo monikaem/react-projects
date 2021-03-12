d32795c582afcb235972d55626ce8a33614c7e84
Monika <monika.machula94@gmail.com>
2021-03-12 14:59:10 +0100

    add Cocktail project

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+        <h1 className='section-title'>about us</h1>
+        <p>An easy way to increase your cocktail sales is to give the people what they want. And what people want is out there. First, we’ve got some great lists of easy, profitable, and popular cocktail recipes based around ingredient seasonality. Check out our easy winter cocktails, summer cocktails, spring cocktails, and fall cocktails. All standardized recipes, easy to make.
+
+            Second, there’s invaluable market research done by reputable market research firms just floating around out there. All based on the most popular cocktails. The kind of drinks every bartender should know. If you’re in the business of selling cocktails for money, you have to look at research like that. It’s literally indispensable.</p>
+    </section>
   )
 }
 
